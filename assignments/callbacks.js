68f15edfc42dce0c434fb2d53800c82b1b3824cb
jfkinslow <killajk2201@gmail.com>
2019-02-26 20:36:18 -0600

    Finished with callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
@@ -24,27 +24,50 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function logger(item) {
+  console.log(item);
+}
+
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, logger);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1])
 }
+
+last(items, logger);
+
+// last(items, function(last){
+//   console.log(last);
+// });
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(2, 3, logger);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 3, logger);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
+
+contains("Notebook", items, logger);
 
 /* STRETCH PROBLEM */
 
@@ -52,4 +75,11 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let seen = {};
+  return cb(Array.from(array).filter((item) => {
+    return seen.hasOwnProperty(item) ? false: (seen[item] = true);
+  }));
 }
+logger(items);
+removeDuplicates(items, logger);
+logger(items);
